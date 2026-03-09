2acaa12a60dd628775bc60edb61306883313d9da
vareloglou <89994939+vareloglou@users.noreply.github.com>
2026-03-09 12:20:27 +0200

    Add Home component with centered test heading

---
+++ app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+      <h1>test</h1>
+    </div>
+  );
+}
